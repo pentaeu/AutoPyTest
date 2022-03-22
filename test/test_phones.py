57d0a7527c2ef54f1778a33a77c373375ffa7a92
Pentaeu <device656575@gmail.com>
2022-03-23 00:23:21 +0800

    Добавлен test_all_data

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,25 +1,5 @@
-from random import randrange
 from model.contact import Contact
 import re
-
-
-def test_all_data_on_home_page(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="ContactName", lastname="ContactLastName", mobile_phone="+7934123455",
-                                   work_phone="306312", home_phone="342321", email1="email1@test.com",
-                                   email2="email2@test.com", email3="email3@test.com", address="test_street"))
-    all_data_on_page_home = app.contact.get_contact_list()
-    index = randrange(len(all_data_on_page_home))
-    contact_data_from_home_page = all_data_on_page_home[index]
-    contact_data_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_data_from_home_page.id == contact_data_from_edit_page.id
-    assert contact_data_from_home_page.lastname == contact_data_from_edit_page.lastname
-    assert contact_data_from_home_page.firstname == contact_data_from_edit_page.firstname
-    assert contact_data_from_home_page.address == contact_data_from_edit_page.address
-    assert contact_data_from_home_page.all_emails_from_home_page == \
-           merge_emails_on_edit_page(contact_data_from_edit_page)
-    assert contact_data_from_home_page.all_phones_from_home_page == \
-           merge_phones_on_edit_page(contact_data_from_edit_page)
 
 
 def test_phones_on_home_page(app):
