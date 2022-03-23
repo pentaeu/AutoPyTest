372eb9b3d27a4e196919ceb78df47ea45882fe63
Pentaeu <device656575@gmail.com>
2022-03-23 11:37:56 +0800

    Исправлен __eq__

--- model/contact.py
+++ model/contact.py
@@ -24,22 +24,12 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % \
-                        (self.id, self.firstname, self.lastname, self.middlename, self.nickname, self.title,
-                         self.company, self.address, self.email1, self.email2, self.email3, self.mobile_phone,
-                         self.work_phone, self.home_phone, self.all_emails_from_home_page,
-                         self.all_phones_from_home_page)
+        return "%s:%s:%s" % \
+                        (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
-               and self.firstname == other.firstname and self.lastname == other.lastname \
-               and self.middlename == other.middlename and self.nickname == other.nickname \
-               and self.title == other.title and self.company == other.company and self.address == other.address \
-               and self.email1 == other.email1 and self.email2 == other.email2 and self.email3 == other.email3 \
-               and self.mobile_phone == other.mobile_phone and self.work_phone == other.work_phone \
-               and self.home_phone == other.home_phone \
-               and self.all_emails_from_home_page == other.all_emails_from_home_page \
-               and self.all_phones_from_home_page == other.all_phones_from_home_page
+               and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
